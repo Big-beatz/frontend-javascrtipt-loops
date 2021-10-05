bbf1545d59eaa89d96598d13de9995da34c29d9c
Big-beatz <kalmaerik@gmail.com>
2021-10-05 16:59:57 +0200

    loops-and-arrays ook af

--- loops-and-arrays.js
+++ loops-and-arrays.js
@@ -5,6 +5,13 @@
 // ==========================================
 
 const names = ["Henk", "Piet", "Fred", "Joop"];
+console.log(names);
+
+for (let i = 0; i < names.length; i++){
+    names[i] = names[i] + "je"
+}
+console.log(names);
+
 
 // Verwachtte uitkomsten:
 // Vóór het script zie je de originele waardes:
@@ -20,6 +27,13 @@ const names = ["Henk", "Piet", "Fred", "Joop"];
 // ==========================================
 
 const numbers = [2, 4, 5, 29, 38];
+console.log(numbers);
+
+for (let i = 0 ; i < numbers.length ; i++){
+    numbers[i] = numbers[i] * 3;
+}
+
+console.log(numbers);
 
 // Verwachtte uitkomsten:
 // Vóór het script zie je de originele waardes:
@@ -36,9 +50,17 @@ const numbers = [2, 4, 5, 29, 38];
 // ==========================================
 
 const squares = [30, 2, 8, 24, 11];
+console.log(squares);
+
+for (let i = 0 ; i < squares.length ; i++){
+    squares[i] = squares[i] ** 3;
+}
+
+console.log(squares);
 
 // Verwachtte uitkomsten:
 // Vóór het script zie je de originele waardes:
 // console.log(numbers) geeft: [30, 2, 8, 24, 11];
 // Na jouw script zie je de aangepaste waardes:
 // console.log(numbers) geeft: [27000, 8, 512, 13824, 1331];
+
